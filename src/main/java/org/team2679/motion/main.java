40660e5032934cfd012aded99cad2f003274bb30
SlowL0ris <yuval.friedman@yahoo.com>
2018-07-20 15:09:29 +0300

    added application display beta

--- src/main/java/org/team2679/motion/main.java
+++ src/main/java/org/team2679/motion/main.java
@@ -1,32 +1,10 @@
 package org.team2679.motion;
 
-import org.knowm.xchart.QuickChart;
-import org.knowm.xchart.SwingWrapper;
-import org.knowm.xchart.XYChart;
-import org.team2679.gui.Visualizer;
-import org.team2679.motion.spline.Cubic;
+import org.team2679.gui.Display;
 import org.team2679.motion.spline.Spline;
-
-import java.sql.SQLOutput;
 
 public class main {
     public static void main(String args[]){
-        double startTime = System.nanoTime();
-
-        Spline spline = new Spline(new Waypoint[]{
-                new Waypoint(0,0,0),
-                new Waypoint(10,10,70),
-                new Waypoint(11, 20 , 90),
-                new Waypoint(7, 25 , 120)
-        }, Spline.SPLINE_TYPE.QUINTIC);
-
-        Waypoint[] path = spline.getSamples(5000);
-
-        double now = System.nanoTime();
-
-        System.out.println((now-startTime)/1e6);
-
-        Visualizer v = new Visualizer(path);
-        v.visualize();
+        Display.startApplication();
     }
 }
--- src/main/java/org/team2679/motion/spline/Spline.java
+++ src/main/java/org/team2679/motion/spline/Spline.java
@@ -5,8 +5,6 @@ import org.team2679.motion.Waypoint;
 public class Spline {
 
     Polynomial[] splines;
-    private double length = 0;
-    private double xLength = 0;
 
     private SPLINE_TYPE type;
 
@@ -15,26 +13,30 @@ public class Spline {
     }
 
     public Spline(Waypoint[] waypoints, SPLINE_TYPE type){
-        this.splines = new Polynomial[waypoints.length - 1];
-        if(type == SPLINE_TYPE.CUBIC)
-        {
+        if(waypoints.length <= 1){
+            this.splines = new Polynomial[0];
+        }
+        else {
             this.splines = new Polynomial[waypoints.length - 1];
-            for(int i = 0; i < waypoints.length - 1; i++){
-                splines[i] = new Cubic(waypoints[i], waypoints[i + 1]);
+            if (type == SPLINE_TYPE.CUBIC) {
+                this.splines = new Polynomial[waypoints.length - 1];
+                for (int i = 0; i < waypoints.length - 1; i++) {
+                    splines[i] = new Cubic(waypoints[i], waypoints[i + 1]);
+                }
+            } else if (type == SPLINE_TYPE.QUINTIC) {
+                this.splines = new Quintic[waypoints.length - 1];
+                for (int i = 0; i < waypoints.length - 1; i++) {
+                    splines[i] = new Quintic(waypoints[i], waypoints[i + 1]);
+                }
             }
         }
-        else if (type == SPLINE_TYPE.QUINTIC)
-        {
-            this.splines = new Quintic[waypoints.length - 1];
-            for(int i = 0; i < waypoints.length - 1; i++){
-                splines[i] = new Quintic(waypoints[i], waypoints[i + 1]);
-            }
-        }
-
         this.type = type;
     }
 
     public Waypoint[] getSamples(int amount){
+        if(this.splines.length < 1){
+            return null;
+        }
         Waypoint[] path = new Waypoint[amount*splines.length];
         for(int i = 0; i < splines.length; i++) {
             double u = 0;
